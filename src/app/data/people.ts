2347287402b74874e4ae2d71ee29f355833a0bb3
Dany Rolando Álava Santana <dany04072000@gmail.com>
2023-08-30 08:55:50 -0500

    Primer commit

---
+++ src/app/data/people.ts
@@ -0,0 +1,182 @@
+export const People = [
+      {
+        id: 1,
+        name: 'Persona 1',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 1',
+        'company-image': 'empresa1.png',
+        levelOfHappiness: 85
+      },
+      {
+        id: 2,
+        name: 'Persona 2',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 2',
+        'company-image': 'empresa2.png',
+        levelOfHappiness: 72
+      },
+      {
+        id: 3,
+        name: 'Persona 3',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 3',
+        'company-image': 'empresa3.png',
+        levelOfHappiness: 94
+      },
+      {
+        id: 4,
+        name: 'Persona 4',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 4',
+        'company-image': 'empresa4.png',
+        levelOfHappiness: 60
+      },
+      {
+        id: 5,
+        name: 'Persona 5',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 5',
+        'company-image': 'empresa5.png',
+        levelOfHappiness: 78
+      },
+      {
+        id: 6,
+        name: 'Persona 6',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 6',
+        'company-image': 'empresa6.png',
+        levelOfHappiness: 88
+      },
+      {
+        id: 7,
+        name: 'Persona 7',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 7',
+        'company-image': 'empresa7.png',
+        levelOfHappiness: 42
+      },
+      {
+        id: 8,
+        name: 'Persona 8',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 8',
+        'company-image': 'empresa8.png',
+        levelOfHappiness: 96
+      },
+      {
+        id: 9,
+        name: 'Persona 9',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 9',
+        'company-image': 'empresa9.png',
+        levelOfHappiness: 75
+      },
+      {
+        id: 10,
+        name: 'Persona 10',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 10',
+        'company-image': 'empresa10.png',
+        levelOfHappiness: 63
+      },
+      {
+        id: 11,
+        name: 'Persona 11',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 11',
+        'company-image': 'empresa11.png',
+        levelOfHappiness: 89
+      },
+      {
+        id: 12,
+        name: 'Persona 12',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 12',
+        'company-image': 'empresa12.png',
+        levelOfHappiness: 58
+      },
+      {
+        id: 13,
+        name: 'Persona 13',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 13',
+        'company-image': 'empresa13.png',
+        levelOfHappiness: 71
+      },
+      {
+        id: 14,
+        name: 'Persona 14',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 14',
+        'company-image': 'empresa14.png',
+        levelOfHappiness: 80
+      },
+      {
+        id: 15,
+        name: 'Persona 15',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 15',
+        'company-image': 'empresa15.png',
+        levelOfHappiness: 55
+      },
+      {
+        id: 16,
+        name: 'Persona 16',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 16',
+        'company-image': 'empresa16.png',
+        levelOfHappiness: 92
+      },
+      {
+        id: 17,
+        name: 'Persona 17',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 17',
+        'company-image': 'empresa17.png',
+        levelOfHappiness: 68
+      },
+      {
+        id: 18,
+        name: 'Persona 18',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 18',
+        'company-image': 'empresa18.png',
+        levelOfHappiness: 77
+      },
+      {
+        id: 19,
+        name: 'Persona 19',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 19',
+        'company-image': 'empresa19.png',
+        levelOfHappiness: 87
+      },
+      {
+        id: 20,
+        name: 'Persona 20',
+        category: 'manager',
+        'category-image': 'manager.png',
+        company: 'Empresa 20',
+        'company-image': 'empresa20.png',
+        levelOfHappiness: 50
+      }
+];
