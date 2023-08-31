7b56d283d405699d7a40c6581829fd3781b0c28f
Dany Rolando Álava Santana <dany04072000@gmail.com>
2023-08-31 16:34:06 -0500

    toolbar added

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { PeopleTableComponent } from '../people-table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule],
+  imports: [
+    MatToolbarModule, 
+    MatIconModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatBadgeModule
+  ],
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
+
 export class ToolbarComponent {
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(PeopleTableComponent, {
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
 
 }
